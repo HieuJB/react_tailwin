4cb8991e98775923d399c245668c92e0c5b37aa3
HieuJB <trunghieuit061099@gmail.com>
2021-03-27 20:56:22 +0700

    update_code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import './assets/css/dist/tailwind.css';
+
+function App() {
+  return (
+    <div className="bg-gray-200">
+      <section className="">
+      <buton className="btn">dasdsa</buton>
+      </section>
+    </div>
+  );
+}
+
+export default App;
